d9f541babd2a6959b81f5698faf620000e922c58
andhika-m <andhikamalik291@gmail.com>
2022-03-28 17:54:00 +0700

    task 2

--- Tugas-2/BasicPython-Tugas2.py
+++ Tugas-2/BasicPython-Tugas2.py
@@ -1,6 +1,6 @@
 # Author: Andhika Malik
 
-print("Selamat datang!\n")
+print("\nSelamat datang!\n")
 print("=====================")
 print("\tMenu")
 print("=====================")
@@ -9,52 +9,70 @@ print("2. Tambah Kontak")
 print("3. Keluar")
 print("---------------------")
 
-menu = int(input("Masukan pilihan : "))
 daftar_kontak = [
     {'Nama':'Fawwaz', 'Telp':'081234567890'}, 
     {'Nama':'Jhon', 'Telp':'089876543210'}]
 
-if menu == 1:
-    print('\n==============================')
-    print("\tDaftar kontak:")
-    print('==============================')
-    
-    for dk in daftar_kontak:
-        print('Nama\t\t:', dk['Nama'])
-        print('No Telepon \t:', dk['Telp'], '\n')
+def pilihan_menu():
+    pilih = int(input("Masukan menu : "))
+    menu_telp(pilih)
 
-elif menu == 2:
-    i = 1
-    baru = [{}]
-    d = []
-    banyak = int(input('Berapa banyak kontak yang ingin ditambahkan : '))
-    
-    while i <= banyak:
-        print('\n- Kontak ke', i)
-        print('-----------------------------')
-        nama = input('Masukan Nama\t\t: ')
-        notlp = input('Masukan No Telepon \t: ')
+def menu_telp(menu):
+    if menu == 1:
+        print('\n==============================')
+        print("\tDaftar kontak:")
+        print('==============================')
         
-        data1 = baru[0]['Nama'] = nama
-        data2 = baru[0]['Telp'] = notlp
+        for dk in daftar_kontak:
+            print('Nama\t\t:', dk['Nama'])
+            print('No Telepon \t:', dk['Telp'], '\n')
         
-        d.append({'Nama':data1, 'Telp':data2})
-        i += 1
+        kembali = input('Ingin memilih menu lain (y/t) ? ')
+        if kembali == 'y' or kembali == 'Y':
+            pilihan_menu()
+        else:
+            print('Program selesai, sampai jumpa!\n')
+            
+    elif menu == 2:
+        i = 1
+        baru = [{}]
+        d = []
+        banyak = int(input('Berapa banyak kontak yang ingin ditambahkan : '))
+        
+        while i <= banyak:
+            print('\n- Kontak ke', i)
+            print('-----------------------------')
+            nama = input('Masukan Nama\t\t: ')
+            notlp = input('Masukan No Telepon \t: ')
+            
+            data1 = baru[0]['Nama'] = nama
+            data2 = baru[0]['Telp'] = notlp
+            
+            d.append({'Nama':data1, 'Telp':data2})
+            i += 1
 
-    print('\n=============================')
-    print('Kontak berhasil ditambahkan!')
-    print('=============================')
-    for br in d:
-        print('-Baru-')
-        print('Nama\t\t:', br['Nama'])
-        print('No Telepon \t:', br['Telp'], '\n')
+        print('\n=============================')
+        print('Kontak berhasil ditambahkan!')
+        print('=============================')
+        for br in d:
+            print('-Baru-')
+            print('Nama\t\t:', br['Nama'])
+            print('No Telepon \t:', br['Telp'], '\n')
 
-    for dk in daftar_kontak:
-        print('Nama\t\t:', dk['Nama'])
-        print('No Telepon \t:', dk['Telp'], '\n')
+        for dk in daftar_kontak:
+            print('Nama\t\t:', dk['Nama'])
+            print('No Telepon \t:', dk['Telp'], '\n')
+        
+        kembali = input('Ingin memilih menu lain (y/t) ? ')
+        if kembali == 'y' or kembali == 'Y':
+            pilihan_menu()
+        else:
+            print('Program selesai, sampai jumpa!\n')
 
-elif menu == 3:
-    print('Program selesai, sampai jumpa!')
+    elif menu == 3:
+        print('Program selesai, sampai jumpa!\n')
 
-else:
-    print('Menu tidak tersedia')
+    else:
+        print('Menu tidak tersedia\n')
+
+pilihan_menu()
